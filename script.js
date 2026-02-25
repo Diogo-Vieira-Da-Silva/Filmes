9435ee035e34a1e6c0aa0d3abb7cd4cffcccc667
DIOGO VIEIRA DA SILVA <05708376007@senacrs.edu.br>
2026-02-25 11:40:10 -0300

    Feat: adicionando a api do site

---
+++ script.js
@@ -0,0 +1,7 @@
+ //curl --request GET \
+     //--url https://api.themoviedb.org/3/authentication \
+     //--header 'Authorization: Bearer 8e594be3048bf6c34bf75715defcb721' \
+     //--header 'accept: application/json'
+
+     const getMovies = async () => {
+        fetch('https://api.themoviedb.org/3/discover/movie?api_key=8e594be3048bf6c34bf75715defcb721')}
